fe804b9bea78a446b56acb0ee5c2d73309f64483
Indah71 <indahjumiatin7@gmail.com>
2025-12-19 11:46:02 +0800

    urls and views

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home
 
 urlpatterns = [
+    path('', home, name='home'),   # 👈 root /
     path('admin/', admin.site.urls),
 ]
+
